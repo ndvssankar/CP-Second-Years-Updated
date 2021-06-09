021ac526f6b0758adfc4c55c8fade0ad51fa01dd
ndvssankar <sivashankar@msitprogram.net>
2021-06-09 17:43:07 +0530

    test runner updated

--- 09-isperfectsquare-Java/TestJUnit.java
+++ 09-isperfectsquare-Java/TestJUnit.java
@@ -1,11 +1,27 @@
-// isPerfectSquare(n)
-// Write the function isPerfectSquare(n) that takes a possibly-non-int value, and returns True if
-// it is an int that is a perfect square (that is, if there exists an integer m such that
-// m**2 == n), and False otherwise. Do not crash on non-ints nor on negative ints.
+/**
+ * This is JUnit that tests the sleepIn method that is available in SleepIn class.
+ * This contains 4 testcases.
+ * 
+ * Please don't run this file.
+ * You can add your own test cases to this file by just copy and paste the last three 
+ * lines of the code (TestCase4).
+ * 
+ * @author: Siva Sankar
+ */
 
-public class IsPerfectSquare {
-	public boolean isPerfectSquare(int n) {
-		// Your code goes here
-		return false;
+import org.junit.Test;
+import java.util.*;
+import static org.junit.Assert.assertEquals;
+
+public class TestJUnit {
+   @Test
+	public void testCase1() {
+		assertEquals("1.", false, new IsPerfectSquare().isPerfectSquare(-625));
+		assertEquals("2.", true, new IsPerfectSquare().isPerfectSquare(625));
+		assertEquals("3.", true, new IsPerfectSquare().isPerfectSquare(4));
+		assertEquals("4.", true, new IsPerfectSquare().isPerfectSquare(100));
+		assertEquals("5.", false, new IsPerfectSquare().isPerfectSquare(1100));
+		assertEquals("6.", false, new IsPerfectSquare().isPerfectSquare(-100));
+		assertEquals("7.", true, new IsPerfectSquare().isPerfectSquare(0));
 	}
 }

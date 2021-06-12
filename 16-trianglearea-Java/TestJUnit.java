f6d1feba29d8e72de14e815a8ae94017ddfdd6cc
ndvssankar <sivashankar@msitprogram.net>
2021-06-12 10:41:59 +0530

    test runner updated

--- 16-trianglearea-Java/TestJUnit.java
+++ 16-trianglearea-Java/TestJUnit.java
@@ -16,14 +16,14 @@ public class TestJUnit {
     TriangleArea triangleArea = new TriangleArea();
    @Test
    public void testCase1() {
-      assertEquals(24, triangleArea.triangleArea(4.0, 13.0, 15.0));
-      assertEquals(216, triangleArea.triangleArea(24.0, 18.0, 30.0));
-      assertEquals(6, triangleArea.triangleArea(4.0, 5.0, 3.0));
+      assertEquals("1.", 24, triangleArea.triangleArea(4.0, 13.0, 15.0));
+      assertEquals("2.", 216, triangleArea.triangleArea(24.0, 18.0, 30.0));
+      assertEquals("3.", 6, triangleArea.triangleArea(4.0, 5.0, 3.0));
    }
 
    @Test
    public void testCase2() {
-      assertEquals(43, triangleArea.triangleArea(10.0, 10.0, 10.0));
-      assertEquals(45, triangleArea.triangleArea(10.0, 10.5, 10.0));
+      assertEquals("1.", 43, triangleArea.triangleArea(10.0, 10.0, 10.0));
+      assertEquals("2.", 45, triangleArea.triangleArea(10.0, 10.5, 10.0));
    }
 }
--- 16-trianglearea-Java/TriangleArea.java
+++ 16-trianglearea-Java/TriangleArea.java
@@ -1,6 +1,6 @@
 // trianglearea(s1, s2, s3)[5pts]
 // Write the function trianglearea(s1, s2, s3) that takes 3 floats/ints and returns the area of the
-// triangle that has those lengths of its side. If no such triangle exists, return 0. Hint: you
+// triangle that has those lengths of its side as an integer. If no such triangle exists, return 0. Hint: you
 // will probably wish to use Heron's Formula.
 
 

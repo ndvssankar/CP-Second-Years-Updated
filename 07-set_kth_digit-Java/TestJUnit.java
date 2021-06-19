ffbbbbca5e35848f193c49834e9b6fbe5561b3e8
ndvssankar <sivashankar@msitprogram.net>
2021-06-19 19:03:30 +0530

    description and tr updated

--- 07-set_kth_digit-Java/TestJUnit.java
+++ 07-set_kth_digit-Java/TestJUnit.java
@@ -1,5 +1,5 @@
 /**
- * This is JUnit that tests the methods of the Clock.
+ * This is JUnit that tests the methods of the setKthDigit in MyMath.
  * This contains 2 testcases.
  * 
  * Please don't run this file.
@@ -7,6 +7,7 @@
  * paste the last three lines of the code (TestCase2).
  * 
  * @author: Deepak
+ * @author: Siva Sankar
  */
 
 import org.junit.Test;
@@ -18,17 +19,17 @@ public class TestJUnit {
 
    @Test
    public void testCase1() {
-      set_kth_digit s = new set_kth_digit();
-      assertEquals("1.", 461, s.fun_set_kth_digit(468, 0, 1));
-      assertEquals("2.", 418, s.fun_set_kth_digit(468, 1, 1));
-      assertEquals("3.", 168, s.fun_set_kth_digit(468, 2, 1));
+      MyMath s = new MyMath();
+      assertEquals("1.", 461, s.setKthDigit(468, 0, 1));
+      assertEquals("2.", 418, s.setKthDigit(468, 1, 1));
+      assertEquals("3.", 168, s.setKthDigit(468, 2, 1));
     }
 
    @Test
    public void testCase2() {
-      set_kth_digit s = new set_kth_digit();
-      assertEquals("1.", 1468, s.fun_set_kth_digit(468, 3, 1));
-      assertEquals("2.", -1468, s.fun_set_kth_digit(-468, 3, 1));
+      MyMath s = new MyMath();
+      assertEquals("1.", 1468, s.setKthDigit(468, 3, 1));
+      assertEquals("2.", -1468, s.setKthDigit(-468, 3, 1));
+      assertEquals("1.", 468, s.setKthDigit(468, 5, 1));
    }
 }
-

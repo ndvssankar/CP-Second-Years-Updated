634ce3a7f7aa00c566a6199bcfb307843d920a54
ndvssankar <sivashankar@msitprogram.net>
2021-07-01 15:04:51 +0530

    code comments and class name updated

--- 10-mostfrequencydigit-Java/MyMath.java
+++ 10-mostfrequencydigit-Java/MyMath.java
@@ -2,7 +2,7 @@
 // Write the function mostFrequentDigit(n), that takes a non-negative integer n and returns the digit from 0 to 9 
 // that occurs most frequently in it, with ties going to the smaller digit.
 
-public class MostFrequentDigit {
+public class MyMath {
 	public int mostFrequentDigit(int n) {
 		// Your code goes here
 		return 1;

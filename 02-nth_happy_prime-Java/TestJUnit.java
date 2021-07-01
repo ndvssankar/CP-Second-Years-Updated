0b95c87de5ae0d5c11cd4dc9d93e25bdad52fe71
ndvssankar <sivashankar@msitprogram.net>
2021-07-01 18:58:55 +0530

    Code updated

--- 02-nth_happy_prime-Java/TestJUnit.java
+++ 02-nth_happy_prime-Java/TestJUnit.java
@@ -1,12 +1,14 @@
 /**
- * This is JUnit that tests the methods of the Clock.
+ * This JUnit tests the HappyNumber class methods.
  * This contains 2 testcases.
  * 
  * Please don't run this file.
- * You can add your own test cases to this file by just copy and 
- * paste the last three lines of the code (TestCase2).
+ * You can add your own test cases to this file by just copy 
+ * and paste the testcase2 along with the annotation @test and
+ * change the copied testcase2 to testcase3
  * 
  * @author: Deepak
+ * @author: Siva Sankar
  */
 
 import org.junit.Test;
@@ -18,19 +20,17 @@ public class TestJUnit {
 
    @Test
    public void testCase1() {
-      nth_happy_prime s = new nth_happy_prime();
-      
-      assertEquals("1.", 7, s.fun_nth_happy_prime(0));
-      assertEquals("2.", 13, s.fun_nth_happy_prime(1));
-      assertEquals("3.", 19, s.fun_nth_happy_prime(2));
+      HappyNumber s = new HappyNumber();
+      assertEquals("1.", 7, s.nthHappyPrime(1));
+      assertEquals("2.", 13, s.nthHappyPrime(2));
+      assertEquals("3.", 19, s.nthHappyPrime(3));
    }
 
    @Test
    public void testCase2() {
-      nth_happy_prime s = new nth_happy_prime();
-      
-      assertEquals("1.", 23, s.fun_nth_happy_prime(3));
-      assertEquals("2.", 31, s.fun_nth_happy_prime(4)); 
+      HappyNumber s = new HappyNumber();
+      assertEquals("1.", 23, s.nthHappyPrime(4));
+      assertEquals("2.", 31, s.nthHappyPrime(5)); 
    }
 }
 

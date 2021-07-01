3092c510b242f75fae9aa2f9243573ac43f150b9
ndvssankar <sivashankar@msitprogram.net>
2021-07-01 18:21:29 +0530

    description and testcases updated

--- 01-nth_happy_number-Python/nth_happy_number.py
+++ 01-nth_happy_number-Python/nth_happy_number.py
@@ -1,15 +1,19 @@
-# Write the function nthHappyNumber(n) which takes a non-negative integer 
+# Write the function nth_happy_number(n) which takes a non-negative integer 
 # and returns the nth happy number (where the 0th happy number is 1). 
 # Here are some test assertions for you:
-# assert(nthHappyNumber(0) == 1)
-# assert(nthHappyNumber(1) == 7)
-# assert(nthHappyNumber(2) == 10)
-# assert(nthHappyNumber(3) == 13)
-# assert(nthHappyNumber(4) == 19)
-# assert(nthHappyNumber(5) == 23)
-# assert(nthHappyNumber(6) == 28)
-# assert(nthHappyNumber(7) == 31)
+
+# https://en.wikipedia.org/wiki/Happy_number#:~:text=In%20number%20theory%2C%20a%20happy,starting%20with%20and%20eventually%20reaches
+# Read more about the happy number from the above link.
+
+# assert(nth_happy_number(1) == 1)
+# assert(nth_happy_number(2) == 7)
+# assert(nth_happy_number(3) == 10)
+# assert(nth_happy_number(4) == 13)
+# assert(nth_happy_number(5) == 19)
+# assert(nth_happy_number(6) == 23)
+# assert(nth_happy_number(7) == 28)
+# assert(nth_happy_number(8) == 31)
 
 
-def fun_nth_happy_number(n):
+def nth_happy_number(n):
 	return 0
--- 01-nth_happy_number-Python/test_nth_happy_number.py
+++ 01-nth_happy_number-Python/test_nth_happy_number.py
@@ -1,12 +1,12 @@
 import os,sys
 sys.path.append(os.getcwd())
-from nth_happy_number import fun_nth_happy_number
+from nth_happy_number import nth_happy_number
 import pytest
 
 
 @pytest.mark.parametrize('a, result',[
-    (0,1),(1,7),(2,10),(3,13),(4,19),(5,23),(6,28),(7,31)
+    (1,1),(2,7),(3,10),(4,13),(5,19),(6,23),(7,28),(8,31)
 ])
-def test_fun_nth_happy_number(a, result):
-    assert fun_nth_happy_number(a) == result
+def test_nth_happy_number(a, result):
+    assert nth_happy_number(a) == result
 

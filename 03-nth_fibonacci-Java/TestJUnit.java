26e5c1026950604aead0793fdfb49838ece64f62
ndvssankar <sivashankar@msitprogram.net>
2021-07-01 12:47:43 +0530

    test cases and runner and code udpated

--- 03-nth_fibonacci-Java/TestJUnit.java
+++ 03-nth_fibonacci-Java/TestJUnit.java
@@ -1,12 +1,13 @@
 /**
- * This is JUnit that tests the methods of the Clock.
+ * This JUnit tests the methods of the MyMath class.
  * This contains 2 testcases.
  * 
  * Please don't run this file.
  * You can add your own test cases to this file by just copy and 
- * paste the last three lines of the code (TestCase2).
+ * paste the last three lines of the code (TestCase3).
  * 
  * @author: Deepak
+ * @author: Siva Sankar
  */
 
 import org.junit.Test;
@@ -18,24 +19,24 @@ public class TestJUnit {
 
    @Test
    public void testCase1() {
-      nth_fibonacci s = new nth_fibonacci();
-      assertEquals("1.", 1, s.fun_nthfibonaccinumber(0));
-      assertEquals("2.", 1, s.fun_nthfibonaccinumber(1));
-      assertEquals("3.", 2, s.fun_nthfibonaccinumber(2));
-      assertEquals("4.", 3, s.fun_nthfibonaccinumber(3));
-      assertEquals("5.", 5, s.fun_nthfibonaccinumber(4));
-      assertEquals("6.", 8, s.fun_nthfibonaccinumber(5));
+      MyMath s = new MyMath();
+      assertEquals("1.", 1, s.nthFibonacciNumber(0));
+      assertEquals("2.", 1, s.nthFibonacciNumber(1));
+      assertEquals("3.", 2, s.nthFibonacciNumber(2));
+      assertEquals("4.", 3, s.nthFibonacciNumber(3));
+      assertEquals("5.", 5, s.nthFibonacciNumber(4));
+      assertEquals("6.", 8, s.nthFibonacciNumber(5));
    }
 
    @Test
    public void testCase2() {
-      nth_fibonacci s = new nth_fibonacci();
-      assertEquals("1.", 13, s.fun_nthfibonaccinumber(6));
-      assertEquals("2.", 21, s.fun_nthfibonaccinumber(7));
-      assertEquals("3.", 34, s.fun_nthfibonaccinumber(8));
-      assertEquals("4.", 55, s.fun_nthfibonaccinumber(9));
-      assertEquals("5.", 89, s.fun_nthfibonaccinumber(10));
-      assertEquals("6.", 144, s.fun_nthfibonaccinumber(11));
+      MyMath s = new MyMath();
+      assertEquals("1.", 13, s.nthFibonacciNumber(6));
+      assertEquals("2.", 21, s.nthFibonacciNumber(7));
+      assertEquals("3.", 34, s.nthFibonacciNumber(8));
+      assertEquals("4.", 55, s.nthFibonacciNumber(9));
+      assertEquals("5.", 89, s.nthFibonacciNumber(10));
+      assertEquals("6.", 144, s.nthFibonacciNumber(11));
    }
 }
 

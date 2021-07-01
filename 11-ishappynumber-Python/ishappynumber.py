d03e337da1c3cf39b627a32d1e38385fb39e150a
ndvssankar <sivashankar@msitprogram.net>
2021-07-01 17:30:28 +0530

    Question lines 7 and 8 added updated

--- 11-ishappynumber-Python/ishappynumber.py
+++ 11-ishappynumber-Python/ishappynumber.py
@@ -4,6 +4,9 @@
 # stay ), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 
 # are happy numbers.
 
+# https://en.wikipedia.org/wiki/Happy_number#:~:text=In%20number%20theory%2C%20a%20happy,starting%20with%20and%20eventually%20reaches
+# Read more about the happy number from the above link.
+
 # Write the function ishappynumber(n) which takes a possibly-negative integer and returns True if it is happy and 
 # False otherwise. Note that all numbers less than 1 are not happy. Here are some test assertions for you:
 # assert(ishappynumber(-7) == False)

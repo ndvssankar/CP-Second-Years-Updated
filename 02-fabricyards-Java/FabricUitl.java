546e97f9a50b84b300ca3f6e2cb05b2039c4b8a4
ndvssankar <sivashankar@msitprogram.net>
2021-07-01 12:31:28 +0530

    code updated

--- 02-fabricyards-Java/FabricUitl.java
+++ 02-fabricyards-Java/FabricUitl.java
@@ -1,25 +1,25 @@
-// fabricyards(inches)
+// fabricYards(inches)
 // Fabric must be purchased in whole yards, so purchasing just 1 inch 
 // of fabric requires purchasing 1 entire yard. With this in mind, 
 // write the function fabricYards(inches) that takes the number of 
 // inches of fabric desired, and returns the smallest number of whole 
 // yards of fabric that must be purchased.
 
-// fabricexcess(inches)
+// fabricExcess(inches)
 // Write the function fabricExcess(inches) that takes the number of 
 // inches of fabric desired and returns the number of inches of excess 
-// fabric that must be purchased (as purchases must be in whole yards)
-// . Hint: you may want to use fabricYards, which you just wrote!
+// fabric that must be purchased (as purchases must be in whole yards).
+// Hint: you may want to use fabricYards, which you just wrote!
 
 
 class FabricUitl {
 	public int fabricYards(int inches) {
-		// your code goes here
+		// Your code goes here...
 		return 0;
 	}
 
 	public int fabricExcess(int inches) {
-		// your code goes here
+		// Your code goes here...
 		return 0;
 	}
 }

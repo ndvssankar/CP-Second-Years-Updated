dc2e3c592ab92243cdf91109eebbb9a98785f7d9
ndvssankar <sivashankar@msitprogram.net>
2021-07-01 12:24:45 +0530

    test runner updated

--- 01-find_int_roots-Java/TestJUnit.java
+++ 01-find_int_roots-Java/TestJUnit.java
@@ -1,5 +1,5 @@
 /**
- * This is JUnit that tests the methods of the Clock.
+ * This JUnit tests the methods of the Clock.
  * This contains 2 testcases.
  * 
  * Please don't run this file.
@@ -18,24 +18,24 @@ public class TestJUnit {
 
    @Test
    public void testCase1() {
-      find_int_roots s = new find_int_roots();
+      MyMath s = new MyMath();
       int[] a1 = {2,3};
       int[] a2 = {2,4};
       int[] a3 = {-4,3};
-
-      assertArrayEquals("1.", a1, s.fun_find_int_roots(1,-5, 6));
-      assertArrayEquals("2.", a2, s.fun_find_int_roots(1, -6, 8));
-      assertArrayEquals("3.", a3, s.fun_find_int_roots(1, 1, -12));
+      
+      assertArrayEquals("1.", a1, s.find_int_roots(1,-5, 6));
+      assertArrayEquals("2.", a2, s.find_int_roots(1, -6, 8));
+      assertArrayEquals("3.", a3, s.find_int_roots(1, 1, -12));
    }
 
    @Test
    public void testCase2() {
-      find_int_roots s = new find_int_roots();
+      MyMath s = new MyMath();
       int[] a1 = {1, 5};
       int[] a2 = {-2, -1};
 
-      assertArrayEquals("1.", a1, s.fun_find_int_roots(1,-6, 5));
-      assertArrayEquals("2.", a2, s.fun_find_int_roots(1, 3, 2)); 
+      assertArrayEquals("1.", a1, s.find_int_roots(1,-6, 5));
+      assertArrayEquals("2.", a2, s.find_int_roots(1, 3, 2)); 
    }
 }
 

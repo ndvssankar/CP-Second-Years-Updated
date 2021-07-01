2ac0d3089a5da640e91e75b077f43e0ef69b9235
ndvssankar <sivashankar@msitprogram.net>
2021-07-01 17:22:59 +0530

    updated

--- 11-ishappynumber-Java/HappyNumber.java
+++ 11-ishappynumber-Java/HappyNumber.java
@@ -3,7 +3,8 @@
 // number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will 
 // stay ), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 
 // are happy numbers.
-
+// https://en.wikipedia.org/wiki/Happy_number#:~:text=In%20number%20theory%2C%20a%20happy,starting%20with%20and%20eventually%20reaches
+// Read more about the happy number from the above link.
 // Write the function ishappynumber(n) which takes a possibly-negative integer and returns True if it is happy and 
 // False otherwise. Note that all numbers less than 1 are not happy. Here are some test assertions for you:
 // assert(ishappynumber(-7) == False)
@@ -14,7 +15,7 @@
 // assert(ishappynumber(404) == True)
 // assert(ishappynumber(405) == False)
 
-public class IsHappyNumber {
+public class HappyNumber {
 
 	public boolean isHappyNumber(int n) {
 		// your code goes here
--- 11-ishappynumber-Java/TestJUnit.java
+++ 11-ishappynumber-Java/TestJUnit.java
@@ -1,10 +1,11 @@
 /**
- * This is JUnit that tests the sleepIn method that is available in SleepIn class.
- * This contains 4 testcases.
+ * This JUnit tests the MyMath class methods.
+ * This contains 2 testcases.
  * 
  * Please don't run this file.
- * You can add your own test cases to this file by just copy and paste the last three 
- * lines of the code (TestCase4).
+ * You can add your own test cases to this file by just copy 
+ * and paste the testcase2 along with the annotation @test and
+ * change the copied testcase2 to testcase3
  * 
  * @author: Siva Sankar
  */
@@ -16,17 +17,17 @@ import static org.junit.Assert.assertEquals;
 public class TestJUnit {
    @Test
    public void testCase1() {
-		assertEquals("1.", false, new IsHappyNumber().isHappyNumber(-7));
-      assertEquals("2.", true, new IsHappyNumber().isHappyNumber(1));
-      assertEquals("3.", false, new IsHappyNumber().isHappyNumber(2));
-      assertEquals("4.", false, new IsHappyNumber().isHappyNumber(98));
+		assertEquals("1.", false, new HappyNumber().isHappyNumber(-7));
+      assertEquals("2.", true, new HappyNumber().isHappyNumber(1));
+      assertEquals("3.", false, new HappyNumber().isHappyNumber(2));
+      assertEquals("4.", false, new HappyNumber().isHappyNumber(98));
    }
 
    @Test
    public void testCase2() {
-      assertEquals("1.", false, new IsHappyNumber().isHappyNumber(405));
-      assertEquals("2.", true, new IsHappyNumber().isHappyNumber(97));
-      assertEquals("3.", true, new IsHappyNumber().isHappyNumber(404));
-      assertEquals("4.", false, new IsHappyNumber().isHappyNumber(0));
+      assertEquals("1.", false, new HappyNumber().isHappyNumber(405));
+      assertEquals("2.", true, new HappyNumber().isHappyNumber(97));
+      assertEquals("3.", true, new HappyNumber().isHappyNumber(404));
+      assertEquals("4.", false, new HappyNumber().isHappyNumber(0));
    }
 }

adddbfc5cbd311f017b214ac1f3a86bc92330198
ndvssankar <sivashankar@msitprogram.net>
2021-07-07 10:48:13 +0530

    test runner updated

--- 05-interleave-Java/TestJUnit.java
+++ 05-interleave-Java/TestJUnit.java
@@ -1,13 +1,16 @@
 /**
- * This is JUnit that tests the methods of the Clock.
+ * This JUnit tests the MyString class methods.
  * This contains 2 testcases.
  * 
  * Please don't run this file.
- * You can add your own test cases to this file by just copy and 
- * paste the last three lines of the code (TestCase2).
+ * You can add your own test cases to this file by just copy 
+ * and paste the testcase2 along with the annotation @test and
+ * change the copied testcase2 to testcase3
  * 
  * @author: Deepak
+ * @author: Siva Sankar
  */
+
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
@@ -18,7 +21,7 @@ public class TestJUnit {
 
    @Test
    public void testCase1() {
-      Interleave s = new Interleave();
+      MyString s = new MyString();
       
       assertEquals("1.", "python", s.interleave("pto", "yhn"));
       assertEquals("2.",  "ac#D!f2", s.interleave("a#", "cD!f2"));
@@ -27,7 +30,7 @@ public class TestJUnit {
 
    @Test
    public void testCase2() {
-      Interleave s = new Interleave();
+      MyString s = new MyString();
       
       assertEquals("1.", "ac#De!23r3f", s.interleave("a#e23r3f", "cD!"));
       assertEquals("2.", "ac#Ds!ffg2jsfdeel klsddjlf", s.interleave("a#sfgjfeel klsddjlf", "cD!f2sd")); 

35cdb120cd72310472674ce807739ffb2911a6b2
ndvssankar <sivashankar@msitprogram.net>
2021-07-07 10:39:40 +0530

    updated testrunner and comments in junit

--- 03-applycaesarcipher-Java/TestJUnit.java
+++ 03-applycaesarcipher-Java/TestJUnit.java
@@ -1,13 +1,16 @@
 /**
- * This is JUnit that tests the methods of the Clock.
+ * This JUnit tests the CeaserCipher class methods.
  * This contains 2 testcases.
  * 
  * Please don't run this file.
- * You can add your own test cases to this file by just copy and 
- * paste the last three lines of the code (TestCase2).
+ * You can add your own test cases to this file by just copy 
+ * and paste the testcase2 along with the annotation @test and
+ * change the copied testcase2 to testcase3
  * 
  * @author: Deepak
+ * @author: Siva Sankar
  */
+
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
@@ -18,7 +21,7 @@ public class TestJUnit {
 
    @Test
    public void testCase1() {
-      applycaesarcipher s = new applycaesarcipher();
+      CeaserCipher s = new CeaserCipher();
       
       assertEquals("1.", "Xf Buubdl Bu Ebxo", s.fun_applycaesarcipher("We Attack At Dawn", 1));
       assertEquals("2.",  "xmbgya", s.fun_applycaesarcipher("zodiac", -2));
@@ -27,7 +30,7 @@ public class TestJUnit {
 
    @Test
    public void testCase2() {
-      applycaesarcipher s = new applycaesarcipher();
+      CeaserCipher s = new CeaserCipher();
       
       assertEquals("1.", "DEFGABC", s.fun_applycaesarcipher("ABCDXYZ", 3));
       assertEquals("2.", "xyzauvw", s.fun_applycaesarcipher("abcdxyz", -3)); 

017250ee54e1aa66e812ef327728e2baba39ef34
ndvssankar <sivashankar@msitprogram.net>
2021-07-07 17:12:47 +0530

    test runner

--- 07-removeduplicate-Java/TestJUnit.java
+++ 07-removeduplicate-Java/TestJUnit.java
@@ -1,11 +1,13 @@
 /**
- * This is JUnit that tests the sleepIn method that is available in SleepIn class.
- * This contains 4 testcases.
+ * This JUnit tests the MyString class methods.
+ * This contains 2 testcases.
  * 
  * Please don't run this file.
- * You can add your own test cases to this file by just copy and paste the last three 
- * lines of the code (TestCase4).
+ * You can add your own test cases to this file by just copy 
+ * and paste the testcase2 along with the annotation @test and
+ * change the copied testcase2 to testcase3
  * 
+ * @author: Deepak
  * @author: Siva Sankar
  */
 
@@ -16,19 +18,19 @@ import static org.junit.Assert.assertEquals;
 public class TestJUnit {
    @Test
    public void testCase1() {
-		assertEquals("1.", "JavPython", new RemoveDuplicate().removeDuplicate("JavaPython"));
-      assertEquals("2.", "HeloWrd", new RemoveDuplicate().removeDuplicate("HelloWorld"));
-      assertEquals("3.", "E", new RemoveDuplicate().removeDuplicate("EEE"));
-      assertEquals("4.", "a ", new RemoveDuplicate().removeDuplicate("a a "));
+		assertEquals("1.", "JavPython", new MyString().removeDuplicate("JavaPython"));
+      assertEquals("2.", "HeloWrd", new MyString().removeDuplicate("HelloWorld"));
+      assertEquals("3.", "E", new MyString().removeDuplicate("EEE"));
+      assertEquals("4.", "a ", new MyString().removeDuplicate("a a "));
    }
 
    @Test
    public void testCase2() {
-      assertEquals("1.", "12", new RemoveDuplicate().removeDuplicate("121212121"));
-      assertEquals("2.",  "", new RemoveDuplicate().removeDuplicate(""));
-      assertEquals("3.", "Test", new RemoveDuplicate().removeDuplicate("Test"));
-      assertEquals("4.", "10", new RemoveDuplicate().removeDuplicate("1001"));
-      assertEquals("5.", "10", new RemoveDuplicate().removeDuplicate("11110000"));
+      assertEquals("1.", "12", new MyString().removeDuplicate("121212121"));
+      assertEquals("2.",  "", new MyString().removeDuplicate(""));
+      assertEquals("3.", "Test", new MyString().removeDuplicate("Test"));
+      assertEquals("4.", "10", new MyString().removeDuplicate("1001"));
+      assertEquals("5.", "10", new MyString().removeDuplicate("11110000"));
    }
 }
 

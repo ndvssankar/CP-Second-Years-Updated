93801d88897c273e19e2233ad6da82bb96cfedfa
ndvssankar <sivashankar@msitprogram.net>
2021-07-07 10:44:57 +0530

    test runner updated

--- 04-vowelscount-Java/TestJUnit.java
+++ 04-vowelscount-Java/TestJUnit.java
@@ -1,13 +1,16 @@
 /**
- * This is JUnit that tests the methods of the Clock.
+ * This JUnit tests the MyString class methods.
  * This contains 2 testcases.
  * 
  * Please don't run this file.
- * You can add your own test cases to this file by just copy and 
- * paste the last three lines of the code (TestCase2).
+ * You can add your own test cases to this file by just copy 
+ * and paste the testcase2 along with the annotation @test and
+ * change the copied testcase2 to testcase3
  * 
  * @author: Deepak
+ * @author: Siva Sankar
  */
+
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
@@ -18,19 +21,19 @@ public class TestJUnit {
 
    @Test
    public void testCase1() {
-      vowelscount s = new vowelscount();
+      MyString s = new MyString();
       
-      assertEquals("1.", 3, s.fun_vowelscount("Abc def!!! a? yzyzyz!"));
-      assertEquals("2.",  3, s.fun_vowelscount("zodiac"));
-      assertEquals("3.", 1, s.fun_vowelscount("ABCDXYZ"));
+      assertEquals("1.", 3, s.vowelCount("Abc def!!! a? yzyzyz!"));
+      assertEquals("2.",  3, s.vowelCount("zodiac"));
+      assertEquals("3.", 1, s.vowelCount("ABCDXYZ"));
    }
 
    @Test
    public void testCase2() {
-      vowelscount s = new vowelscount();
+      MyString s = new MyString();
       
-      assertEquals("1.", 4, s.fun_vowelscount("Hell123ooo"));
-      assertEquals("2.", 10, s.fun_vowelscount("aeiouAeiou  ")); 
+      assertEquals("1.", 4, s.vowelCount("Hell123ooo"));
+      assertEquals("2.", 10, s.vowelCount("aeiouAeiou  ")); 
    }
 }
 

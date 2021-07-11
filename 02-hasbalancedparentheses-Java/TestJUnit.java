86d3eb67ae2ca8d5ce1577dee5f7c6ad1b14bca2
ndvssankar <sivashankar@msitprogram.net>
2021-07-11 23:18:16 +0530

    updated

--- 02-hasbalancedparentheses-Java/TestJUnit.java
+++ 02-hasbalancedparentheses-Java/TestJUnit.java
@@ -1,11 +1,12 @@
 /**
  * This is JUnit that tests the sleepIn method that is available in SleepIn class.
- * This contains 4 testcases.
+ * This contains 1 testcases.
  * 
  * Please don't run this file.
  * You can add your own test cases to this file by just copy and paste the last three 
  * lines of the code (TestCase4).
  * 
+ * @author: Deeapk
  * @author: Siva Sankar
  */
 
@@ -15,7 +16,9 @@ import static org.junit.Assert.assertEquals;
 public class TestJUnit {
    @Test
    public void testCase1() {
-      assertEquals(new PythonBasics().show_excitement(), "I am super excited for this course! I am super excited for this course! I am super excited for this course! I am super excited for this course! I am super excited for this course! ");
+      assertEquals("1.", true, new BalancedParenthesis().isParenthesisBalanced("()"));
+      assertEquals("2.", false, new BalancedParenthesis().isParenthesisBalanced("((())"));
+      assertEquals("3.", false, new BalancedParenthesis().isParenthesisBalanced(")))(((("));
+      assertEquals("4.", true, new BalancedParenthesis().isParenthesisBalanced(""));
    }
-
 }

6af9d653f7a980cffec103f20b74e100b66a6f9c
ndvssankar <sivashankar@msitprogram.net>
2021-07-11 23:07:26 +0530

    updated

--- 01-nthtenlyprime-Java/TestJUnit.java
+++ 01-nthtenlyprime-Java/TestJUnit.java
@@ -1,10 +1,10 @@
 /**
- * This is JUnit that tests the sleepIn method that is available in SleepIn class.
- * This contains 4 testcases.
+ * This is JUnit that tests the tenlyprime method that is available in Prime class.
+ * This contains 1 testcases.
  * 
  * Please don't run this file.
  * You can add your own test cases to this file by just copy and paste the last three 
- * lines of the code (TestCase4).
+ * lines of the code (TestCase1).
  * 
  * @author: Siva Sankar
  */
@@ -15,7 +15,10 @@ import static org.junit.Assert.assertEquals;
 public class TestJUnit {
    @Test
    public void testCase1() {
-      assertEquals(new PythonBasics().show_excitement(), "I am super excited for this course! I am super excited for this course! I am super excited for this course! I am super excited for this course! I am super excited for this course! ");
+      assertEquals("1.", 19, new Prime().nthTenlyPrime(1));
+      assertEquals("2.", 37, new Prime().nthTenlyPrime(2));
+      assertEquals("3.", 73, new Prime().nthTenlyPrime(3));
+      assertEquals("4.", 109, new Prime().nthTenlyPrime(4));
+      assertEquals("5.", 127, new Prime().nthTenlyPrime(5));
    }
-
 }

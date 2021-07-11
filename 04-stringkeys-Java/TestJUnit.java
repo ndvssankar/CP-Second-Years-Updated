e877c76e8746eb452d669b0802b5300326248e00
ndvssankar <sivashankar@msitprogram.net>
2021-07-11 17:34:03 +0530

    updat3ed

--- 04-stringkeys-Java/TestJUnit.java
+++ 04-stringkeys-Java/TestJUnit.java
@@ -6,6 +6,7 @@
  * You can add your own test cases to this file by just copy and paste the last three 
  * lines of the code (TestCase4).
  * 
+ * @author: Deepak
  * @author: Siva Sankar
  */
 
@@ -16,30 +17,29 @@ public class TestJUnit {
    @Test
    public void testCase1() {
    	StringKeys s = new StringKeys();
-   	assertEquals(s.calculate_hash_value("UDACITY"), 8568);
-   	assertEquals(s.lookup("UDACITY"), -1);
+   	assertEquals("1.", 8568, s.calculate_hash_value("UDACITY"));
+   	assertEquals("2.", -1, s.lookup("UDACITY"));
    }
    @Test
    public void testCase2() {
    	StringKeys s = new StringKeys();
-   	assertEquals(s.calculate_hash_value("UDACITY"), 8568);
-   	assertEquals(s.lookup("UDACITY"), -1);
+   	assertEquals("1.", 8568, s.calculate_hash_value("UDACITY"));
+   	assertEquals("2.", -1, s.lookup("UDACITY"));
    	s.store("UDACITY");
-   	assertEquals(s.lookup("UDACITY"), 8568);
+   	assertEquals("3.", 8568, s.lookup("UDACITY"));
    }
    @Test
    public void testCase3() {
    	StringKeys s = new StringKeys();
-   	assertEquals(s.calculate_hash_value("UDACIOUS"), 8568);
-   	assertEquals(s.lookup("UDACIOUS"), -1);
+   	assertEquals("1.", 8568, s.calculate_hash_value("UDACIOUS"));
+   	assertEquals("2.", -1, s.lookup("UDACIOUS"));
    }
    @Test
    public void testCase4() {
    	StringKeys s = new StringKeys();
-   	assertEquals(s.calculate_hash_value("UDACIOUS"), 8568);
-   	assertEquals(s.lookup("UDACIOUS"), -1);
+   	assertEquals("1.", 8568, s.calculate_hash_value("UDACIOUS"));
+   	assertEquals("2.", -1, s.lookup("UDACIOUS"));
    	s.store("UDACIOUS");
-   	assertEquals(s.lookup("UDACIOUS"), 8568);
+   	assertEquals("3.", 8568, s.lookup("UDACIOUS"));
    }
-   
 }

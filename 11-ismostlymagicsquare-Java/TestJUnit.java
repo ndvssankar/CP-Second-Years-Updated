d5b12eed0dda4408130a7d87ac408b22a4e1c11f
ndvssankar <sivashankar@msitprogram.net>
2021-07-11 17:40:30 +0530

    updated

--- 11-ismostlymagicsquare-Java/TestJUnit.java
+++ 11-ismostlymagicsquare-Java/TestJUnit.java
@@ -1,5 +1,5 @@
 /**
- * This is JUnit that tests the sleepIn method that is available in SleepIn class.
+ * This is JUnit that tests the isMostlyMagicSquare method that is available in IsMostlyMagicSquare class.
  * This contains 4 testcases.
  * 
  * Please don't run this file.
@@ -15,9 +15,8 @@ import static org.junit.Assert.assertEquals;
 public class TestJUnit {
    @Test
    public void testCase1() {
-      assertEquals(new IsMostlyMagicSquare().isMostlyMagicSquare(new int[][]{{2, 7, 6}, {9, 5, 1}, {4, 3, 8}}), true);
-      assertEquals(new IsMostlyMagicSquare().isMostlyMagicSquare(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}), false);
-      assertEquals(new IsMostlyMagicSquare().isMostlyMagicSquare(new int[][]{{16, 3, 2, 13}, {5, 10, 11, 8}, {9, 6, 7, 12},{4, 15, 14, 1}}), true);
+      assertEquals("1.", true, new IsMostlyMagicSquare().isMostlyMagicSquare(new int[][]{{2, 7, 6}, {9, 5, 1}, {4, 3, 8}}));
+      assertEquals("2.", false, new IsMostlyMagicSquare().isMostlyMagicSquare(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}));
+      assertEquals("3.", true, new IsMostlyMagicSquare().isMostlyMagicSquare(new int[][]{{16, 3, 2, 13}, {5, 10, 11, 8}, {9, 6, 7, 12},{4, 15, 14, 1}}));
    }
-
 }

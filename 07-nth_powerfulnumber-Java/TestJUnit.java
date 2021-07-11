3fc17d610d26fc7dc5ac891afc1abf076fb2541b
ndvssankar <sivashankar@msitprogram.net>
2021-07-11 17:44:22 +0530

    updated

--- 07-nth_powerfulnumber-Java/TestJUnit.java
+++ 07-nth_powerfulnumber-Java/TestJUnit.java
@@ -1,10 +1,10 @@
 /**
- * This is JUnit that tests the sleepIn method that is available in SleepIn class.
+ * This is JUnit that tests the nthPowerfulNumber method that is available in NthPowerfulNumber class.
  * This contains 4 testcases.
  * 
  * Please don't run this file.
  * You can add your own test cases to this file by just copy and paste the last three 
- * lines of the code (TestCase4).
+ * lines of the code (TestCase1).
  * 
  * @author: Siva Sankar
  */
@@ -15,21 +15,20 @@ import static org.junit.Assert.assertEquals;
 public class TestJUnit {
    @Test
    public void testCase1() {
-      	assertEquals(new NthPowerfulNumber().nthPowerfulNumber(53), 1000);
-		assertEquals(new NthPowerfulNumber().nthPowerfulNumber(39), 576);
-		assertEquals(new NthPowerfulNumber().nthPowerfulNumber(29), 343);
-		assertEquals(new NthPowerfulNumber().nthPowerfulNumber(17), 128);
-		assertEquals(new NthPowerfulNumber().nthPowerfulNumber(18), 144);
-		assertEquals(new NthPowerfulNumber().nthPowerfulNumber(19), 169);
-		assertEquals(new NthPowerfulNumber().nthPowerfulNumber(4), 16);
-		assertEquals(new NthPowerfulNumber().nthPowerfulNumber(5), 25);
-		assertEquals(new NthPowerfulNumber().nthPowerfulNumber(6), 27);
-		assertEquals(new NthPowerfulNumber().nthPowerfulNumber(7), 32);
-		assertEquals(new NthPowerfulNumber().nthPowerfulNumber(8), 36);
-		assertEquals(new NthPowerfulNumber().nthPowerfulNumber(9), 49);
-		assertEquals(new NthPowerfulNumber().nthPowerfulNumber(10), 64);
-		assertEquals(new NthPowerfulNumber().nthPowerfulNumber(0), 1);
+      	assertEquals("1.", 1000, new NthPowerfulNumber().nthPowerfulNumber(53));
+		assertEquals("2.", 576, new NthPowerfulNumber().nthPowerfulNumber(39));
+		assertEquals("3.", 343, new NthPowerfulNumber().nthPowerfulNumber(29));
+		assertEquals("4.", 128, new NthPowerfulNumber().nthPowerfulNumber(17));
+		assertEquals("5.", 144, new NthPowerfulNumber().nthPowerfulNumber(18));
+		assertEquals("6.", 169, new NthPowerfulNumber().nthPowerfulNumber(19));
+		assertEquals("7.", 16, new NthPowerfulNumber().nthPowerfulNumber(4));
+		assertEquals("8.", 25, new NthPowerfulNumber().nthPowerfulNumber(5));
+		assertEquals("9.", 27, new NthPowerfulNumber().nthPowerfulNumber(6));
+		assertEquals("10.", 32, new NthPowerfulNumber().nthPowerfulNumber(7));
+		assertEquals("11.", 36, new NthPowerfulNumber().nthPowerfulNumber(8));
+		assertEquals("12.", 49, new NthPowerfulNumber().nthPowerfulNumber(9));
+		assertEquals("13.", 64, new NthPowerfulNumber().nthPowerfulNumber(10));
+		assertEquals("14.", 1, new NthPowerfulNumber().nthPowerfulNumber(0));
    }
-
 }
 

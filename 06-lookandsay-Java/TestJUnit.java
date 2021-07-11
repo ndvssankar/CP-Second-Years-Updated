0d71962d54bfb9faa5c2c880d37522f9a61853bd
ndvssankar <sivashankar@msitprogram.net>
2021-07-11 17:37:18 +0530

    updated

--- 06-lookandsay-Java/TestJUnit.java
+++ 06-lookandsay-Java/TestJUnit.java
@@ -15,14 +15,14 @@ import static org.junit.Assert.assertEquals;
 public class TestJUnit {
    @Test
    public void testCase1() {
-      assertEquals(new LookAndSay().lookAndSay(new int[0]),"[]");
-      assertEquals(new LookAndSay().lookAndSay(new int[]{1, 1, 1}), "[(3, 1)]");
-      assertEquals(new LookAndSay().lookAndSay(new int[] {-1, 2, 7}), "[(1, -1), (1, 2), (1, 7)]");
+      assertEquals("1.", "[]", new LookAndSay().lookAndSay(new int[0]));
+      assertEquals("2.", "[(3, 1)]", new LookAndSay().lookAndSay(new int[]{1, 1, 1}));
+      assertEquals("3.", "[(1, -1), (1, 2), (1, 7)]", new LookAndSay().lookAndSay(new int[] {-1, 2, 7}));
    }
 
    @Test
    public void testCase2() {
-      assertEquals(new LookAndSay().lookAndSay(new int[]{3, 3, 8, -10, -10, -10}), "[(2, 3), (1, 8), (3, -10)]");
-      assertEquals(new LookAndSay().lookAndSay(new int[] {3, 3, 8, 3, 3, 3, 3}),"[(2, 3), (1, 8), (4, 3)]");
+      assertEquals("1.", "[(2, 3), (1, 8), (3, -10)]", new LookAndSay().lookAndSay(new int[]{3, 3, 8, -10, -10, -10}));
+      assertEquals("2.", "[(2, 3), (1, 8), (4, 3)]", new LookAndSay().lookAndSay(new int[] {3, 3, 8, 3, 3, 3, 3}));
    }
 }

84d46a4a7bd0fc7287c43b31de230f5bb47564ce
ndvssankar <sivashankar@msitprogram.net>
2021-07-12 00:06:09 +0530

    updated

--- 05-nthwithproperty309-Java/TestJUnit.java
+++ 05-nthwithproperty309-Java/TestJUnit.java
@@ -1,10 +1,10 @@
 /**
- * This is JUnit that tests the sleepIn method that is available in SleepIn class.
- * This contains 4 testcases.
+ * This is JUnit that tests the method that is available in NthWithProperty309 class.
+ * This contains 3 testcases.
  * 
  * Please don't run this file.
  * You can add your own test cases to this file by just copy and paste the last three 
- * lines of the code (TestCase4).
+ * lines of the code (TestCase3).
  * 
  * @author: Siva Sankar
  */
@@ -15,20 +15,20 @@ import static org.junit.Assert.assertEquals;
 public class TestJUnit {
    @Test
    public void testCase1() {
-        assertEquals(new NthWithProperty309().nthWithProperty309(0), 309);
-        assertEquals(new NthWithProperty309().nthWithProperty309(418), 1);
-        assertEquals(new NthWithProperty309().nthWithProperty309(462), 2);
-        assertEquals(new NthWithProperty309().nthWithProperty309(474), 3);
+        assertEquals("1,", 309, new NthWithProperty309().nthWithProperty309(0));
+        assertEquals("2,", 1, new NthWithProperty309().nthWithProperty309(418));
+        assertEquals("3,", 2, new NthWithProperty309().nthWithProperty309(462));
+        assertEquals("4,", 3, new NthWithProperty309().nthWithProperty309(474));
    }
    @Test
    public void testCase2() {
-        assertEquals(new NthWithProperty309().nthWithProperty309(575), 4);
-        assertEquals(new NthWithProperty309().nthWithProperty309(635), 5);
-        assertEquals(new NthWithProperty309().nthWithProperty309(662), 6);
-        assertEquals(new NthWithProperty309().nthWithProperty309(2014), 100);
+        assertEquals("1,", 4, new NthWithProperty309().nthWithProperty309(575));
+        assertEquals("2,", 5, new NthWithProperty309().nthWithProperty309(635));
+        assertEquals("3,", 6, new NthWithProperty309().nthWithProperty309(662));
+        assertEquals("4,", 100, new NthWithProperty309().nthWithProperty309(2014));
    }
    @Test
    public void testCase3() {
-        assertEquals(new NthWithProperty309().nthWithProperty309(7813), 1000);
+        assertEquals("1.", 1000, new NthWithProperty309().nthWithProperty309(7813));
    }
 }

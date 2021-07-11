d5c77be00b81c55a67f7e8961f719d9f135d18d9
ndvssankar <sivashankar@msitprogram.net>
2021-07-11 22:18:19 +0530

    updated

--- 07-inverselookandsay-Java/TestJUnit.java
+++ 07-inverselookandsay-Java/TestJUnit.java
@@ -15,14 +15,14 @@ import static org.junit.Assert.assertArrayEquals;
 public class TestJUnit {
    @Test
    public void testCase1() {
-      assertArrayEquals(new InverseLookAndSay().inverseLookAndSay("[]"), new int[0]);
-      assertArrayEquals(new InverseLookAndSay().inverseLookAndSay("[(3, 1)]"), new int[]{1, 1, 1});
-      assertArrayEquals(new InverseLookAndSay().inverseLookAndSay("[(1, -1), (1, 2), (1, 7)]"), new int[]{-1, 2, 7});
+      assertArrayEquals("1.", new int[0], new InverseLookAndSay().inverseLookAndSay("[]"));
+      assertArrayEquals("2.", new int[]{1, 1, 1}, new InverseLookAndSay().inverseLookAndSay("[(3, 1)]"));
+      assertArrayEquals("3.", new int[]{-1, 2, 7}, new InverseLookAndSay().inverseLookAndSay("[(1, -1), (1, 2), (1, 7)]"));
    }
 
    @Test
    public void testCase2() {
-      assertArrayEquals(new InverseLookAndSay().inverseLookAndSay("[(2, 3), (1, 8), (3, -10)]"), new int[]{3, 3, 8, -10, -10, -10});
-      assertArrayEquals(new InverseLookAndSay().inverseLookAndSay("[(2, 3), (1, 8), (4, 3)]"), new int[]{3, 3, 8, 3, 3, 3, 3});
+      assertArrayEquals("1.", new int[]{3, 3, 8, -10, -10, -10}, new InverseLookAndSay().inverseLookAndSay("[(2, 3), (1, 8), (3, -10)]"));
+      assertArrayEquals("2.", new int[]{3, 3, 8, 3, 3, 3, 3}, new InverseLookAndSay().inverseLookAndSay("[(2, 3), (1, 8), (4, 3)]"));
    }
 }

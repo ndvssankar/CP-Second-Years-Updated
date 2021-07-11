6a4b828cb87f23d2c858d79439d1b0372a318e68
ndvssankar <sivashankar@msitprogram.net>
2021-07-12 00:14:35 +0530

    updated

--- 06-sumofsquaresofdigits-Java/TestJUnit.java
+++ 06-sumofsquaresofdigits-Java/TestJUnit.java
@@ -1,10 +1,10 @@
 /**
- * This is JUnit that tests the sleepIn method that is available in SleepIn class.
- * This contains 4 testcases.
+ * This is JUnit that tests the method that is available in SumOfSquaresOfDigits class.
+ * This contains 1 testcase.
  * 
  * Please don't run this file.
  * You can add your own test cases to this file by just copy and paste the last three 
- * lines of the code (TestCase4).
+ * lines of the code (TestCase1).
  * 
  * @author: Siva Sankar
  */
@@ -15,16 +15,14 @@ import static org.junit.Assert.assertEquals;
 public class TestJUnit {
    @Test
    public void testCase1() {
-		assertEquals(new SumOfSquaresOfDigits().sumOfSquaresOfDigits(5), 25);
-		assertEquals(new SumOfSquaresOfDigits().sumOfSquaresOfDigits(12), 5);
-		assertEquals(new SumOfSquaresOfDigits().sumOfSquaresOfDigits(234), 29);
-		assertEquals(new SumOfSquaresOfDigits().sumOfSquaresOfDigits(234101), 31);
-		assertEquals(new SumOfSquaresOfDigits().sumOfSquaresOfDigits(2340), 29);
-		assertEquals(new SumOfSquaresOfDigits().sumOfSquaresOfDigits(10), 1);
-		assertEquals(new SumOfSquaresOfDigits().sumOfSquaresOfDigits(12012), 10);
-		assertEquals(new SumOfSquaresOfDigits().sumOfSquaresOfDigits(1), 1);
-		assertEquals(new SumOfSquaresOfDigits().sumOfSquaresOfDigits(0), 0);
+		assertEquals("1.", 25, new SumOfSquaresOfDigits().sumOfSquaresOfDigits(5));
+		assertEquals("2.", 5, new SumOfSquaresOfDigits().sumOfSquaresOfDigits(12));
+		assertEquals("3.", 29, new SumOfSquaresOfDigits().sumOfSquaresOfDigits(234));
+		assertEquals("4.", 31, new SumOfSquaresOfDigits().sumOfSquaresOfDigits(234101));
+		assertEquals("5.", 29, new SumOfSquaresOfDigits().sumOfSquaresOfDigits(2340));
+		assertEquals("6.", 1, new SumOfSquaresOfDigits().sumOfSquaresOfDigits(10));
+		assertEquals("7.", 10, new SumOfSquaresOfDigits().sumOfSquaresOfDigits(12012));
+		assertEquals("8.", 1, new SumOfSquaresOfDigits().sumOfSquaresOfDigits(1));
+		assertEquals("9.", 0, new SumOfSquaresOfDigits().sumOfSquaresOfDigits(0));
    }
-
 }
-

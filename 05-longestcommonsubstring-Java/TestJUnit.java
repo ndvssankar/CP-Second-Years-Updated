72344f3f6fbe6de88779cc8adc311da3a2d2e577
ndvssankar <sivashankar@msitprogram.net>
2021-07-11 23:27:43 +0530

    updated

--- 05-longestcommonsubstring-Java/TestJUnit.java
+++ 05-longestcommonsubstring-Java/TestJUnit.java
@@ -1,10 +1,10 @@
 /**
- * This is JUnit that tests the sleepIn method that is available in SleepIn class.
- * This contains 4 testcases.
+ * This is JUnit that tests the LCS method that is available in LCS class.
+ * This contains 1 testcases.
  * 
  * Please don't run this file.
  * You can add your own test cases to this file by just copy and paste the last three 
- * lines of the code (TestCase4).
+ * lines of the code (TestCase1).
  * 
  * @author: Siva Sankar
  */
@@ -15,15 +15,13 @@ import static org.junit.Assert.assertEquals;
 public class TestJUnit {
    @Test
    public void testCase1() {
-		assertEquals(new LongestCommonSubString().longestCommonSubstring("abcdef", "abqrcdest"), "cde");
-		assertEquals(new LongestCommonSubString().longestCommonSubstring("abcdef", "ghi"), "");
-		assertEquals(new LongestCommonSubString().longestCommonSubstring("abABC", "abcAB"), "AB");
-		assertEquals(new LongestCommonSubString().longestCommonSubstring("abcdef", "abqrcdest"),  "cde");
-		assertEquals(new LongestCommonSubString().longestCommonSubstring("abcdef", "ghi"),  "");
-		assertEquals(new LongestCommonSubString().longestCommonSubstring("", "abqrcdest"),  "");
-		assertEquals(new LongestCommonSubString().longestCommonSubstring("abcdef", ""),  "");
-		assertEquals(new LongestCommonSubString().longestCommonSubstring("abcABC", "zzabZZAB"),  "AB");
+		assertEquals("1.", "cde", new LongestCommonSubString().longestCommonSubstring("abcdef", "abqrcdest"));
+		assertEquals("2.", "", new LongestCommonSubString().longestCommonSubstring("abcdef", "ghi"));
+		assertEquals("3.", "AB", new LongestCommonSubString().longestCommonSubstring("abABC", "abcAB"));
+		assertEquals("4.", "cde", new LongestCommonSubString().longestCommonSubstring("abcdef", "abqrcdest"));
+		assertEquals("5.", "", new LongestCommonSubString().longestCommonSubstring("abcdef", "ghi"));
+		assertEquals("6.", "", new LongestCommonSubString().longestCommonSubstring("", "abqrcdest"));
+		assertEquals("7.", "", new LongestCommonSubString().longestCommonSubstring("abcdef", ""));
+		assertEquals("8.", "AB", new LongestCommonSubString().longestCommonSubstring("abcABC", "zzabZZAB"));
    }
-
 }
-

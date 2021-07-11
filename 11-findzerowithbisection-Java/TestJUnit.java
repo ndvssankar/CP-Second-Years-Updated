358d6bf059b54d545b2cddc6c9f40bf716732d5e
ndvssankar <sivashankar@msitprogram.net>
2021-07-11 17:29:39 +0530

    updated

--- 11-findzerowithbisection-Java/TestJUnit.java
+++ 11-findzerowithbisection-Java/TestJUnit.java
@@ -1,11 +1,12 @@
 /**
- * This is JUnit that tests the sleepIn method that is available in SleepIn class.
+ * This is JUnit that tests the bisection method that is available in Bisection class.
  * This contains 4 testcases.
  * 
  * Please don't run this file.
  * You can add your own test cases to this file by just copy and paste the last three 
  * lines of the code (TestCase4).
  * 
+ * @author: Deepak
  * @author: Siva Sankar
  */
 
@@ -15,9 +16,8 @@ import static org.junit.Assert.assertEquals;
 public class TestJUnit {
    @Test
    public void testCase1() {
-      assertEquals(new Bisection().bisection(25, 0.01, 0.1), 5.00030517578125, 0.00000000000001);
-      assertEquals(new Bisection().bisection(49, 0.01, 0.1), 6.999786376953125, 0.00000000000001);
-      assertEquals(new Bisection().bisection(81, 0.01, 0.1), 9.000274658203125, 0.00000000000001);
+      assertEquals("1.", new Bisection().bisection(25, 0.01, 0.1), 5.00030517578125, 0.00000000000001);
+      assertEquals("2.", new Bisection().bisection(49, 0.01, 0.1), 6.999786376953125, 0.00000000000001);
+      assertEquals("3.", new Bisection().bisection(81, 0.01, 0.1), 9.000274658203125, 0.00000000000001);
    }
-
 }

e8227aa7380fc2535fc9f61186e8af159008e9c7
ndvssankar <sivashankar@msitprogram.net>
2021-07-12 00:04:14 +0530

    updated

--- 04-longestdigitrun-Java/TestJUnit.java
+++ 04-longestdigitrun-Java/TestJUnit.java
@@ -1,10 +1,10 @@
 /**
- * This is JUnit that tests the sleepIn method that is available in SleepIn class.
- * This contains 4 testcases.
+ * This is JUnit that tests the method that is available in LongestDigitRun class.
+ * This contains 2 testcases.
  * 
  * Please don't run this file.
  * You can add your own test cases to this file by just copy and paste the last three 
- * lines of the code (TestCase4).
+ * lines of the code (TestCase2).
  * 
  * @author: Siva Sankar
  */
@@ -15,15 +15,15 @@ import static org.junit.Assert.assertEquals;
 public class TestJUnit {
    @Test
    public void testCase1() {
-      assertEquals(new LongestDigitRun().longestDigitRun(117773732),7);
-      assertEquals(new LongestDigitRun().longestDigitRun(11455567), 5);
-      assertEquals(new LongestDigitRun().longestDigitRun(12345), 1);
+      assertEquals("1.", 7, new LongestDigitRun().longestDigitRun(117773732));
+      assertEquals("2.", 5, new LongestDigitRun().longestDigitRun(11455567));
+      assertEquals("3.", 1, new LongestDigitRun().longestDigitRun(12345));
    }
 
    @Test
    public void testCase2() {
-      assertEquals(new LongestDigitRun().longestDigitRun(-677886), 7);
-      assertEquals(new LongestDigitRun().longestDigitRun(44332211), 1);
-      assertEquals(new LongestDigitRun().longestDigitRun(3330001), 0);
+      assertEquals("1.", 7, new LongestDigitRun().longestDigitRun(-677886));
+      assertEquals("2.", 1, new LongestDigitRun().longestDigitRun(44332211));
+      assertEquals("3.", 0, new LongestDigitRun().longestDigitRun(3330001));
    }
 }

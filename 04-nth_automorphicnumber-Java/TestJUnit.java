f91bcb5d978cbc6c59564e70e3b2378a40e0afbc
ndvssankar <sivashankar@msitprogram.net>
2021-07-11 22:59:50 +0530

    updated

--- 04-nth_automorphicnumber-Java/TestJUnit.java
+++ 04-nth_automorphicnumber-Java/TestJUnit.java
@@ -15,34 +15,34 @@ import static org.junit.Assert.assertEquals;
 public class TestJUnit {
    @Test
    public void testCase1() {
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(1), 0);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(2), 1);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(3), 5);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(4), 6);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(5), 25);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(6), 76);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(7), 376);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(8), 625);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(9), 9376);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(10), 90625);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(11), 109376);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(12), 890625);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(13), 2890625);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(14), 7109376);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(15), 12890625);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(16), 87109376);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(17), 212890625);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(18), 787109376);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(19), 1787109376);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(20), 8212890625L);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(21), 18212890625L);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(22), 81787109376L);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(23), 918212890625L);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(24), 9918212890625L);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(25), 40081787109376L);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(26), 59918212890625L);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(27), 259918212890625L);
-		assertEquals(new NthAutomorphicNumbers().nthAutomorphicNumbers(28), 740081787109376L);
+		assertEquals("1.", 0, new NthAutomorphicNumbers().nthAutomorphicNumbers(1));
+		assertEquals("1.", 1, new NthAutomorphicNumbers().nthAutomorphicNumbers(2));
+		assertEquals("1.", 5, new NthAutomorphicNumbers().nthAutomorphicNumbers(3));
+		assertEquals("1.", 6, new NthAutomorphicNumbers().nthAutomorphicNumbers(4));
+		assertEquals("1.", 25, new NthAutomorphicNumbers().nthAutomorphicNumbers(5));
+		assertEquals("1.", 76, new NthAutomorphicNumbers().nthAutomorphicNumbers(6));
+		assertEquals("1.", 376, new NthAutomorphicNumbers().nthAutomorphicNumbers(7));
+		assertEquals("1.", 625, new NthAutomorphicNumbers().nthAutomorphicNumbers(8));
+		assertEquals("1.", 9376, new NthAutomorphicNumbers().nthAutomorphicNumbers(9));
+		assertEquals("1.", 90625, new NthAutomorphicNumbers().nthAutomorphicNumbers(10));
+		assertEquals("1.", 109376, new NthAutomorphicNumbers().nthAutomorphicNumbers(11));
+		assertEquals("1.", 890625, new NthAutomorphicNumbers().nthAutomorphicNumbers(12));
+		assertEquals("1.", 2890625, new NthAutomorphicNumbers().nthAutomorphicNumbers(13));
+		assertEquals("1.", 7109376, new NthAutomorphicNumbers().nthAutomorphicNumbers(14));
+		assertEquals("1.", 12890625, new NthAutomorphicNumbers().nthAutomorphicNumbers(15));
+		assertEquals("1.", 87109376, new NthAutomorphicNumbers().nthAutomorphicNumbers(16));
+		assertEquals("1.", 212890625, new NthAutomorphicNumbers().nthAutomorphicNumbers(17));
+		assertEquals("1.", 787109376, new NthAutomorphicNumbers().nthAutomorphicNumbers(18));
+		assertEquals("1.", 1787109376, new NthAutomorphicNumbers().nthAutomorphicNumbers(19));
+		assertEquals("1.", 8212890625L, new NthAutomorphicNumbers().nthAutomorphicNumbers(20));
+		assertEquals("1.", 18212890625L, new NthAutomorphicNumbers().nthAutomorphicNumbers(21));
+		assertEquals("1.", 81787109376L, new NthAutomorphicNumbers().nthAutomorphicNumbers(22));
+		assertEquals("1.", 918212890625L, new NthAutomorphicNumbers().nthAutomorphicNumbers(23));
+		assertEquals("1.", 9918212890625L, new NthAutomorphicNumbers().nthAutomorphicNumbers(24));
+		assertEquals("1.", 40081787109376L, new NthAutomorphicNumbers().nthAutomorphicNumbers(25));
+		assertEquals("1.", 59918212890625L, new NthAutomorphicNumbers().nthAutomorphicNumbers(26));
+		assertEquals("1.", 259918212890625L, new NthAutomorphicNumbers().nthAutomorphicNumbers(27));
+		assertEquals("1.", 740081787109376L, new NthAutomorphicNumbers().nthAutomorphicNumbers(28));
    }
 
 }

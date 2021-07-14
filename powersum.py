bb0f7abb308b96e9b629f4df90534c1c474be59d
ndvssankar <sivashankar@msitprogram.net>
2021-07-14 09:09:08 +0530

    updated

--- powersum.py
+++ powersum.py
@@ -3,7 +3,7 @@
 # possibly-negative integers n and k and returns the 
 # so-called power sum: Specify the time complexity based 
 # on the solution that you have given. [Hint: This can be 
-# solved in (n * log k)  or better.
+# solved in (n * log k) or better.
 
 #    1**k + 2**k + ... + n**k
 

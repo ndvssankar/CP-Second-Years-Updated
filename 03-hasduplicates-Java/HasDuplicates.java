99a2206ded78aa51e51bff5448f47042b73748e8
ndvssankar <sivashankar@msitprogram.net>
2021-07-29 09:15:09 +0530

    updated

--- 03-hasduplicates-Java/HasDuplicates.java
+++ 03-hasduplicates-Java/HasDuplicates.java
@@ -1,5 +1,5 @@
 // hasDuplicates(L) [15 pts]
-// Write the function hasDuplicates(L) that takes a 2d list L of arbitrary values, and returns True if L 
+// Write the function hasDuplicates(arr) that takes a 2d arr L of arbitrary values, and returns True if L 
 // contains any duplicate values (that is, 
 // if any two values in L are equal to each other), and False otherwise.
 
